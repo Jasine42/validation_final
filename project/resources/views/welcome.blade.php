4ceb659a7057f2100367edadf42a8c2a94d86216
admin <admin@iMac3.local>
2018-07-05 11:56:15 +0200

    Validation

---
+++ project/resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Bienvenue sur mon site</title>
+    <link rel="stylesheet" href="{{ asset('/css/styles.css') }}">
+  </head>
+    <header style="
+    background-color: cadetblue;
+    height:  100px;
+    display: block;
+    text-align:  center;">
+      <nav>
+        <ul></ul>
+          <li><a class="insertion" href="http://192.168.33.23/posts">Clik ici pour Insérer</a></li>
+      </nav>
+    </header>
+  <body>
+    <main>
+      <section>
+      </section>
+    </main>
+  </body>
+</html>
